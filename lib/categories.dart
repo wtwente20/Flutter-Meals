596cb54cfc67434931c167667103c16e13ea27f9
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-07-29 09:34:22 -0500

    created categories.dart and enabled GridView

---
+++ lib/categories.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your catergory'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          Text('1', style: TextStyle(color: Colors.white),),
+          Text('2', style: TextStyle(color: Colors.white),),
+          Text('3', style: TextStyle(color: Colors.white),),
+          Text('4', style: TextStyle(color: Colors.white),),
+        ],
+      ),
+    );
+  }
+}

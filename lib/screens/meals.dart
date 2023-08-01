4d5f1b0ab4553d3796947e495c3f59799218a8f8
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-07-31 21:57:09 -0500

    modified dummy data, added logic for meals, light styling to category buttons

---
+++ lib/screens/meals.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemBuilder: (ctx, index) => Text(
+        meals[index].title,
+      ),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Uh oh... no meals here!'),
+            const SizedBox(height: 16),
+            Text(
+              'Try selecting a different category!',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
+  }
+}

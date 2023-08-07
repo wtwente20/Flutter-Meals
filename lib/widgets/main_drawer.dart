bba966bf4fec9c4bb0273d27fee226845d835bfc
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-08-07 16:14:00 -0500

    set logic for filters, created first filter

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({
+    super.key,
+    required this.onSelectScreen,
+  });
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +57,9 @@ class MainDrawer extends StatelessWidget {
                   fontSize: 24,
                 ),
           ),
-          onTap: () {},
+          onTap: () {
+            onSelectScreen('meals');
+          },
         ),
         ListTile(
           leading: Icon(
@@ -67,7 +74,9 @@ class MainDrawer extends StatelessWidget {
                   fontSize: 24,
                 ),
           ),
-          onTap: () {},
+          onTap: () {
+            onSelectScreen('filters');
+          },
         ),
       ],
     ));
